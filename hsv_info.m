ad1c0f237d5ea16bebb93021f29dff904896bbd9
j-hydrogen <fligno.int.julius@gmail.com>
2021-10-16 13:47:21 +0800

    added hsv_info function

---
+++ hsv_info.m
@@ -0,0 +1,17 @@
+function hsv_info(rgb_image)
+    %accepts RGB image and displays Histogram of Hue, Sat, and Val
+    
+    hsv_image = rgb2hsv(rgb_image);
+    figure
+    subplot(1,3,1)
+    histogram(hsv_image(:,:,1)*360,360,'DisplayStyle','Stairs','EdgeColor','r')
+    title('HUE Channel')
+    
+    subplot(1,3,2)
+    histogram(hsv_image(:,:,2)*100,100,'DisplayStyle','Stairs','EdgeColor','g')
+    title('SAT Channel')
+    
+    subplot(1,3,3)
+    histogram(hsv_image(:,:,3)*100,100,'DisplayStyle','Stairs','EdgeColor','b')
+    title('VAL Channel')
+end
